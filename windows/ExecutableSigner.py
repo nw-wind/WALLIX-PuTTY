25f1a25c1e75c739edeb9da81ab9caf0361a1eb1
nw-wind <sergei@keler.net>
2020-05-29 22:25:35 +0300

    fix signing

--- windows/ExecutableSigner.py
+++ windows/ExecutableSigner.py
@@ -10,13 +10,22 @@ if not os.path.isfile(target_file_name):
 else:
 	print('target_file_name');
 	
-if os.path.isfile(target_file_name) and                                     \
-   os.path.isfile(sign_cert_file_name) and                                  \
-   os.path.isfile(sign_cert_passwd_file_name):
-    print('ExecutableSigner');
-    with open(sign_cert_passwd_file_name, 'r') as sign_cert_passwd_file:
-        sign_cert_passwd = sign_cert_passwd_file.read().replace('\n', '')
-        subprocess.call(                                                    \
-            ['signtool.exe', 'sign', '/t',                                  \
-             'http://timestamp.digicert.com', '/f', sign_cert_file_name,    \
-             '/fd', 'sha256', '/p', sign_cert_passwd, target_file_name])
+if not os.path.isfile(target_file_name):
+   print('taget is wrong')
+   exit(1)
+                                     
+if not os.path.isfile(sign_cert_file_name):
+   print('cert is wrong')
+   exit(2)                                  
+
+if not os.path.isfile(sign_cert_passwd_file_name):
+   print('password is wrong')
+   exit(3)
+
+print('ExecutableSigner');
+with open(sign_cert_passwd_file_name, 'r') as sign_cert_passwd_file:
+    sign_cert_passwd = sign_cert_passwd_file.read().replace('\n', '')
+    subprocess.call(                                                    \
+        ['C:\\Program Files (x86)\\Windows Kits\\8.1\\bin\\x86\\signtool.exe', 'sign', '/t',                                  \
+         'http://timestamp.digicert.com', '/f', sign_cert_file_name,    \
+         '/fd', 'sha256', '/p', sign_cert_passwd, target_file_name])
