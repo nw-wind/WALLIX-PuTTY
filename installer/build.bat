ffe01472b6a9626e92a6a9f213998827459a6e2b
nw-wind <sergei@keler.net>
2020-06-01 21:55:30 +0300

    with PATH set

--- installer/build.bat
+++ installer/build.bat
@@ -1,1 +1,2 @@
+PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64;C:\Program Files (x86)\Windows Kits\8.1\bin\x86
 devenv /useenv ..\windows\VS2015-WAB\putty.sln /build Release /project Installer.vdproj 
